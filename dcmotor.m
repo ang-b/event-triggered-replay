fa3c899ebc30449134b03ff5d0332e335bafb216
Angelo Barboni <a.barboni16@imperial.ac.uk>
2021-10-07 14:07:45 +0100

    fix: labels; add: new params, more plots

--- dcmotor.m
+++ dcmotor.m
@@ -1,4 +1,5 @@
-if (exist('dcmotorparams.mat', 'file') == 2)
+FORCE_RERUN = true;
+if exist('dcmotorparams.mat', 'file') == 2 && ~FORCE_RERUN
     load('dcmotorparams.mat')
 else
     makedcmotorparams 
@@ -8,23 +9,34 @@ end
 h = @(p) haux(p, Ad, Bd, -K);
 
 for i=1:10
-   abs(eig(h(i))) 
+   abs(eig(h(i)));
 end
 %% simulation
 
 isEvt = false;
-disp("Simulating in event triggering mode");
+disp("Simulating in periodic control mode");
 periodicCtrlSim = sim('dcmotor_sim');
 
-disp("Simulating in periodic control mode");
+disp("Simulating in event-triggered control mode");
 isEvt = true;
 evtCtrlSim = sim('dcmotor_sim');
 
 %%
-figure;
+close all
+figure(1);
+set(1, 'DefaultTextInterpreter', 'latex');
+set(1, 'Unit', 'normalized', 'Position', [0 0 .4 .6]);
+
 subplot(2,1,1)
-plot(periodicCtrlSim.data.getElement('r').Values)
-title("Periodic control");
+plot(periodicCtrlSim.data.getElement('r').Values, 'LineWidth', 1.8)
+title("Periodic control", 'FontSize', 15);
+set(gca, 'TickLabelInterpreter', 'latex', 'FontSize', 12);
+ylabel('$r$');
+
 subplot(2,1,2)
-plot(evtCtrlSim.data.getElement('r').Values)
-title("Event-triggered control");
+plot(evtCtrlSim.data.getElement('r').Values, 'LineWidth', 1.8)
+title("Event-triggered control", 'FontSize', 15);
+set(gca, 'TickLabelInterpreter', 'latex', 'FontSize', 12);
+ylabel('$r$');
+
+% print('-f1', 'comparison.eps', '-depsc2');
--- makedcmotorparams.m
+++ makedcmotorparams.m
@@ -1,19 +1,20 @@
 %% motor parameters
-% J = 10;
-% b = 0.1;
-% 
-% Kt = 10;
-% Ke = Kt/25;
-% 
-% l = 5e-2;   
-% r = 25;
 
-b = 0.019;
-J = 1;
-Ke = 1.1895;
+% parameters from Saab (2001)
+b = 1e-4;
+J = 1.4e-4;
+Ke = 0.556;
 Kt = Ke;
-l = 1.32442e-2;
-r = 3.2645;
+l = 0.658;
+r = 32.91;
+
+% parameter set II
+% b = 0.019;
+% J = 1;
+% Ke = 1.1895;
+% Kt = Ke;
+% l = 1.32442e-2;
+% r = 3.2645;
 
 %% CT state space representation
 % state is [omega; i]
@@ -36,14 +37,16 @@ K = place(Ad, Bd, cpoles);
 
 z = tf('z', Ts);
 
-
 x0 = zeros(2,1);
 
-L = place(Ad', Cd', [0.6, 0.5]).';
+L = place(Ad', Cd', [0.6, 0]).';
 
 %% event-triggering stuff
 
 delta = 1e-2;
-N = 1/(0.784815); % this could be dependent on delta
+N = -1/5.768;
+
+% gain correction for parameter set II
+% N = 1/(0.784815); % this could be dependent on delta
 
 save('dcmotorparams.mat');
