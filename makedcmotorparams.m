2ae938d44d91d3b96c42d2e0ac5bd4de5242fe5d
Angelo Barboni <a.barboni16@imperial.ac.uk>
2021-10-12 16:46:55 +0100

    enh: systematic dc gain correction

--- makedcmotorparams.m
+++ makedcmotorparams.m
@@ -23,7 +23,7 @@ A = [-b/J, Kt/J; -Ke/l, -r/l];
 B = [0; 1/l];
 C = [1, 1];
 
-%% discretization
+%% discretization, control and observer 
 Ts = 1e-3;
 Ad = eye(2) + A*Ts;
 Bd = B*Ts;
@@ -41,12 +41,30 @@ x0 = zeros(2,1);
 
 L = place(Ad', Cd', [0.6, 0]).';
 
-%% event-triggering stuff
-
+% event triggering threshold
 delta = 1e-2;
-N = -1/5.768;
+
+% computing the feed-forward gain adjustment
+sys = ss(Ad, Bd, [Cd; eye(2)], 0, Ts);
+obsv = ss(Ad - L*Cd, [L Bd], eye(2), zeros(2,2), Ts);
+ctrl = ss(0, zeros(1,2) , 1, -K, -1);
+
+ctrl.u = 'uc';
+ctrl.y = 'u';
+
+sys.u = 'u';
+sys.y = {'meas', 'x1', 'x2'};
+
+obsv.u = {'meas', 'u'};
+obsv.y = 'xhat';
+
+sum_ref = sumblk('uc = xhat - xref', 2);
+
+T = connect(ctrl, sys, obsv, sum_ref, 'xref', 'x1');
+N = dcgain(T);
 
 % gain correction for parameter set II
 % N = 1/(0.784815); % this could be dependent on delta
+isEvt = false;
 
-save('dcmotorparams.mat');
+% save('dcmotorparams.mat');
